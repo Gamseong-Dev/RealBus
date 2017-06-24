73961ac98dd18446a8933a5dd883323c296159f4
dajung-park <pdj9715@gmail.com>
2017-06-24 13:20:17 +0900

    범위설정 toggle 기능 추가

--- src/SelectBox.js
+++ src/SelectBox.js
@@ -6,20 +6,25 @@ class SelectBox extends Component {
     super(props)
     this.state={
       selectStyle: 'select',
-      placeholder: '범위 설정하기'
+      placeholder: '범위 설정하기',
+      toggle: false
     }
     this.handleSelected = this.handleSelected.bind(this)
   }
   handleSelected(range){
-    this.setState({ selectStyle: 'select', placeholder: range })
+    this.setState({ selectStyle: 'select', placeholder: range, toggle: !this.state.toggle})
     this.props.handleSetRange(range)
+  }
+  handleIsOpen = () => {
+    this.setState({ selectStyle: 'select is-open', toggle: !this.state.toggle})
+    !this.state.toggle === true ? this.setState({ selectStyle: 'select is-open'}) : this.setState({ selectStyle: 'select'})
   }
   render() {
     const options = [ '1km', '3km', '5km' ];
     let selectStyle = this.state.selectStyle
     return (
         <div className={selectStyle}>
-          <span className="placeholder" onClick={() => this.setState({ selectStyle: 'select is-open'})}>{this.state.placeholder}</span>
+          <span className="placeholder" onClick={this.handleIsOpen}>{this.state.placeholder}</span>
           <ul className="select-ul">
             {options.map((range,i) =>
                  <li
